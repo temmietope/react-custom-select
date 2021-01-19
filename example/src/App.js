b8e7bc3a950e0e3a515285fca71ed5d721351240
temmietope <temmieayodele@gmail.com>
2021-01-19 21:49:37 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-custom-select'
+import 'react-custom-select/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
